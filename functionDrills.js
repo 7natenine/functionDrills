95d83756dd3dfa4a0bf3f79b284708db8bce0cf6
Nathan Leung <nath.leung@gmail.com>
2020-12-26 17:22:56 -0600

    Corrected string interpolation error

--- functionDrills.js
+++ functionDrills.js
@@ -1,6 +1,7 @@
 
 function createGreeting(name, age){ 
-  return "Hi, my name is " + name + " and I'm " + age + " years old"
+  const yearOfBirth = 2020 - age;
+  return `Hi, my name is ${name} and I\'m ${age} years old. I was born in ${yearOfBirth}.`
 }
 
 const greet1 = createGreeting('Chris', 29);
