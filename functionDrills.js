0c492c44e1cabdc6528a6a55b5639a6b81b23317
Nathan Leung <nath.leung@gmail.com>
2020-12-26 17:25:35 -0600

    Created seperate function for year of birth

--- functionDrills.js
+++ functionDrills.js
@@ -1,7 +1,11 @@
 
+function getYearOfBirth(age) {
+  return 2020 - age 
+}
+
 function createGreeting(name, age){ 
-  const yearOfBirth = 2020 - age;
-  return `Hi, my name is ${name} and I\'m ${age} years old. I was born in ${yearOfBirth}.`
+  const yob = getYearOfBirth(age)
+  return `Hi, my name is ${name} and I\'m ${age} years old. I was born in ${yob}.`
 }
 
 const greet1 = createGreeting('Chris', 29);
