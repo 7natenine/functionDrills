a1919d5145a281cf6168153fd2e654f655d3dad7
Nathan Leung <nath.leung@gmail.com>
2020-12-26 17:51:04 -0600

    spelling errors

--- functionDrills.js
+++ functionDrills.js
@@ -1,12 +1,24 @@
 
 function getYearOfBirth(age) {
-  return 2020 - age 
+  if(age>2020||age<0)
+    throw new Error("age can not be negative");
+  return 2020 - age;
 }
 
 function createGreeting(name, age){ 
   const yob = getYearOfBirth(age)
-  return `Hi, my name is ${name} and I\'m ${age} years old. I was born in ${yob}.`
+  return `Hi, my name is ${name} and I\'m ${age} years old. I was born in ${yob}.`;
 }
 
-const greet1 = createGreeting('Chris', 29);
-console.log(greet1)
+function people(name,age){
+  try {
+    const greet = createGreeting(name,age)
+    console.log(greet)
+  }catch (err) { 
+    console.log('Silly Goose! Your age must be wrong!')
+  }
+}
+
+people('Chris', 29);
+people('Dan', -3);
+people('Ben', 2021);
